c56eb3990b55c814afaba93cb04dbc2c39eeb59c
Running Boy in the rain <995586238@qq.com>
2022-03-14 10:57:53 +0800

    debug u220

--- firmware/fx3/b200/firmware/b200_i2c.h
+++ firmware/fx3/b200/firmware/b200_i2c.h
@@ -15,17 +15,23 @@
 /* define B200_VREQ_EEPROM_READ           (uint8_t)(0xBB) */
 
 static uint16_t glI2cPageSize = 0x40;   /* I2C Page size to be used for transfers. */
+//static uint16_t glI2cPageSize = 0x100;   /* I2C Page size to be used for transfers. */
 
 /* This application uses EEPROM as the slave I2C device. The I2C EEPROM
  * part number used is 24LC256. The capacity of the EEPROM is 256K bits */
-#define CY_FX_USBI2C_I2C_MAX_CAPACITY   (32 * 1024) /* Capacity in bytes */
+//#define CY_FX_USBI2C_I2C_MAX_CAPACITY   (32 * 1024) /* Capacity in bytes */
+#define CY_FX_USBI2C_I2C_MAX_CAPACITY   (128 * 1024) /* Capacity in bytes */
 
 /* The following constant is defined based on the page size that the I2C
  * device support. 24LC256 support 64 byte page write access. */
 #define CY_FX_USBI2C_I2C_PAGE_SIZE      (64)
 
+///*microphase*/
+//#define CY_FX_USBI2C_I2C_PAGE_SIZE      (256)
+
 /* I2C Data rate */
 #define CY_FX_USBI2C_I2C_BITRATE        (100000)
+//#define CY_FX_USBI2C_I2C_BITRATE        (400000)
 
 /* Give a timeout value of 5s for any programming. */
 #define CY_FX_USB_I2C_TIMEOUT           (5000)
